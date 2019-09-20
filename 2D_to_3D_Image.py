330ced7e3e5aca241970293c77f09e7ca5dab3dd
KausikN <coe17b010@iiitdm.ac.in>
2019-09-20 22:01:59 +0530

    Added Codes

--- 2D_to_3D_Image.py
+++ 2D_to_3D_Image.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 
-img = cv2.imread('.top-down-image.jpeg')
+f1 = plt.figure(1)
+img = cv2.imread('DisparityL.jpg', 0)
+plt.imshow(img,'gray')
 
 theta = np.deg2rad(20)
 tx = 0
@@ -18,6 +20,7 @@ H = np.array([[C, -S, tx],
               [0,  0, 1]])
 
 # Translation matrix to shift the image center to the origin
+print(img.shape)
 r, c = img.shape
 T = np.array([[1, 0, -c / 2.],
                [0, 1, -r / 2.],
@@ -32,7 +35,10 @@ trans = ProjectiveTransform(T)
 img_rot = warp(img, H)
 img_rot_center_skew = warp(img, S.dot(np.linalg.inv(T).dot(H).dot(T)))
 
+index = 2
 for i in [img, img_rot, img_rot_center_skew]:
-    f, ax = plt.subplots(figsize=(20,20))
-    ax.imshow(i, cmap=plt.cm.gray, interpolation='nearest')
-    plt.show()
+	f1 = plt.figure(index)
+	f, ax = plt.subplots(figsize=(20,20))
+	ax.imshow(i, cmap=plt.cm.gray, interpolation='nearest')
+	index = index + 1
+plt.show()
--- DisparityMap.py
+++ DisparityMap.py
@@ -2,10 +2,26 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-imgL = cv2.imread('tsukuba_l.png',0)
-imgR = cv2.imread('tsukuba_r.png',0)
+print("Started Disparity Map Generator Program....")
 
-stereo = cv2.createStereoBM(numDisparities=16, blockSize=15)
-disparity = stereo.compute(imgL,imgR)
+plt.subplot(3, 1, 1)
+print("Reading Left Image...")
+imgL = cv2.imread('DisparityL.jpg',0)
+plt.imshow(imgL,'gray')
+
+plt.subplot(3, 1, 2)
+print("Reading Right Image...")
+imgR = cv2.imread('DisparityR.jpg',0)
+plt.imshow(imgR,'gray')
+
+print("Finished Reading Images.")
+
+print("Computing Disparity Map...")
+
+stereo = cv2.StereoBM_create(numDisparities=0, blockSize=21)
+disparity = stereo.compute(imgR,imgL)
+
+print("Showing Disparity Map...")
+plt.subplot(3, 1, 3)
 plt.imshow(disparity,'gray')
 plt.show()
